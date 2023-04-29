627f5fa6aabdffb91d41e6b6b84c11a318b06d43
Nazmul Haque <bpinazmul18@gmail.com>
2023-04-29 20:02:32 +0600

    ADD: Add Prettier, ESLint, Husky and Lint-Staged.

---
+++ prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  singleQuote: true,
+  semi: false,
+  printWidth: 80,
+  endOfLine: "auto",
+  arrowParens: "avoid",
+  useTabs: false,
+  bracketSpacing: true,
+};
